c3dfcd26c688944f623cb8fe88a5a0a617c9e6a1
saladsnake <mifrent@hotmail.com>
2018-08-28 20:03:34 +1000

    new boilerplate

--- App.js
+++ App.js
@@ -1,44 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Root from './src/native/index';
+import configureStore from './src/store/index';
 
-/*React Navigator*/
-import { createStackNavigator } from 'react-navigation';
-/*End of React Navigator*/
+const { persistor, store } = configureStore();
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
-
-class HomeScreen extends React.Component {
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text>Open up App.js to start working on your app!</Text>
-                <Text>Changes you make will automatically reload.</Text>
-                <Text>Shake your phone to open the developer menu.</Text>
-            </View>
-        );
-    }
-}
-
-const RootStack = createStackNavigator(
-    {
-        Home: HomeScreen,
-    },
-    {
-        initialRouteName: 'Home',
-    }
-);
-
-export default class App extends React.Component {
-    render() {
-        return (
-            <RootStack/>
-        );
-    }
+export default function App() {
+  return <Root store={store} persistor={persistor} />;
 }
