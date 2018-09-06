d4560ec6faf1fb317a08c23244718ceb1f7dce37
Inci <inci90@users.noreply.github.com>
2018-09-07 07:07:19 +1000

    boilerplate for openshifts

--- Containers/OpenShifts.js
+++ Containers/OpenShifts.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
-import { AppRegistry, View } from 'react-native';
+import { AppRegistry, View, Text } from 'react-native';
 
 export default class OpenShifts extends Component {
   render() {
     return (
       <View>
-        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-        <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
-        <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
+
+        <View style={{width: 400, height: 150, backgroundColor: 'steelblue'}}>
+            <Text>This is a CATERING job and these are details of the job!</Text>
+              <Text>START: 6AM</Text>
+              <Text>END: 11AMPM</Text>
+          </View>
+
+          <View style={{width: 400, height: 150, backgroundColor: 'green'}}>
+              <Text>This is a CLEANING job and these are details of the job!</Text>
+                <Text>START: 9AM</Text>
+                <Text>END: 5PM</Text>
+            </View>
+
+            <View style={{width: 400, height: 150, backgroundColor: 'steelblue'}}>
+                <Text>This is a CATERING job and these are details of the job!</Text>
+                  <Text>START: 2PM</Text>
+                  <Text>END: 7PM</Text>
+              </View>
+
+              <View style={{width: 400, height: 150, backgroundColor: 'green'}}>
+                  <Text>This is a CLEANING job and these are details of the job!</Text>
+                    <Text>START: 8PM</Text>
+                    <Text>END: 11PM</Text>
+                </View>
       </View>
     );
   }
