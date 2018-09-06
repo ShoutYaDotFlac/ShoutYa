e1bf174250ecf67ce56320eb1119b4199e59bcfa
Inci <inci90@users.noreply.github.com>
2018-09-06 12:51:31 +1000

    boilerplate started, login page in profress

--- App.js
+++ App.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, {Component} from "react";
+import {AppRegistry, Text, TextInput, View} from "react-native";
 
 export default class App extends React.Component {
-    render() {
-        return (
-            <Text>This is the app lol</Text>
-        );
-    }
-}
+	constructor(props) {
+		super(props);
+		this.state = {text: ""};
+	}
+	render() {
+		return (
+			<View style={{padding: 10}}>
+				<TextInput
+					style={{height: 40}}
+					placeholder="Type here to translate!"
+					onChangeText={text => this.setState({text})}
+				/>
+				<Text style={{padding: 10, fontSize: 42}}>
+					{this.state.text
+						.split(" ")
+						.map(word => word && "👷")
+						.join(" ")}
+				</Text>
+			</View>
+		); //end of return
+	} //end of render
+} //end of class
