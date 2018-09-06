cb066367ef0c31a2f1da770e9c3586a9e367792f
Boxuan Lu <547181293@qq.com>
2018-09-06 16:37:42 +1000

    add settings.js setting page

--- Containers/Secured.js
+++ Containers/Secured.js
@@ -1,17 +1,46 @@
 import React, {Component} from "react";
 import {ScrollView, Text, View, Button} from "react-native";
 import HomeScreen from './HomeScreen.js';
+import Settings from './Settings.js';
 
 
 export default class Secured extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			pageId: 0
+		};
+
+		this.navToSettings = this.navToSettings.bind(this);
+	}
+
+	navToSettings() {
+		this.setState({pageId: 1});
+	}
+
 	render() {
-		return (
-			<ScrollView style={{padding: 20}}>
-				<Text style={{fontSize: 27}}>Welcome</Text>
-				<View style={{margin: 20}} />
-        <HomeScreen />
-				<Button onPress={this.props.onLogoutPress} title="Logout" />
-			</ScrollView>
-		);
+		switch (this.state.pageId) {
+			case 0: {
+				return (
+					<ScrollView style={{padding: 20}}>
+						<Text style={{fontSize: 27}}>Welcome</Text>
+						<View style={{margin: 20}} />
+						<HomeScreen />
+						<Button onPress={this.navToSettings} title="Settings" />
+					</ScrollView>
+				);
+				break;
+			}
+			case 1: {
+				return (
+					<ScrollView style={{padding: 20}}>
+						<Settings onLogoutPress={this.props.onLogoutPress}/>
+					</ScrollView>
+				)
+				break;
+			}
+		}
+		
 	}
 }
