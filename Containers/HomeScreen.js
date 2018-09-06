3e60bdb39320bcd66e800eaba5ea51cbd91921e6
Inci <inci90@users.noreply.github.com>
2018-09-06 22:43:29 +1000

    cleaned up formatting

--- Containers/HomeScreen.js
+++ Containers/HomeScreen.js
@@ -1,33 +1,31 @@
-import React, { Component } from 'react';
-import {ScrollView, Text, Button} from 'react-native';
-import MyCalendar from './MyCalendar';
-import ShiftInfo from './ShiftInfo';
-
+import React, {Component} from "react";
+import {ScrollView, Text, Button} from "react-native";
+import MyCalendar from "./MyCalendar";
+import ShiftInfo from "./ShiftInfo";
 
 export default class HomeScreen extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedDay: 'Click on a day to view available shifts'
-    }
-    this.selectDay = this.selectDay.bind(this);
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			selectedDay: "Click on a day to view available shifts"
+		};
+		this.selectDay = this.selectDay.bind(this);
+	}
 
-  selectDay(day) {
-    let word;
-    if (typeof day == 'object')
-      this.setState({selectedDay: day.dateString});
-  }
+	selectDay(day) {
+		let word;
+		if (typeof day == "object") this.setState({selectedDay: day.dateString});
+	}
 
-  render() {
-    return (
-      <ScrollView style={{padding: 20}}>
-          <Text style={{fontSize: 30}}>Open Shift</Text>
-          <Text>{this.state.selectedDay}</Text>
-          <MyCalendar selectDay={this.selectDay}/>
-          <ShiftInfo date={this.state.selectedDay}/>
-          <Button onPress={this.props.onProfilePress} title="Profile" />
-      </ScrollView>
-    )
-  }
+	render() {
+		return (
+			<ScrollView style={{padding: 20}}>
+				<Text style={{fontSize: 30}}>Open Shift</Text>
+				<Text>{this.state.selectedDay}</Text>
+				<MyCalendar selectDay={this.selectDay} />
+				<ShiftInfo date={this.state.selectedDay} />
+				<Button onPress={this.props.onProfilePress} title="Profile" />
+			</ScrollView>
+		);
+	}
 }
--- Containers/Login.js
+++ Containers/Login.js
@@ -1,28 +1,16 @@
-import React, { Component } from 'react';
-import {
-    ScrollView,
-    Text,
-    TextInput,
-    View,
-    Button
-} from 'react-native';
+import React, {Component} from "react";
+import {ScrollView, Text, TextInput, View, Button} from "react-native";
 
 export default class Login extends React.Component {
-    render() {
-        return (
-            <ScrollView style={{padding: 20}}>
-                <Text
-                    style={{fontSize: 27}}>
-                    Login
-                </Text>
-                <TextInput placeholder='Username' />
-                <TextInput placeholder='Password' />
-                <View style={{margin:7}} />
-                <Button
-                          onPress={this.props.onLoginPress}
-                          title="Submit"
-                      />
-                  </ScrollView>
-            )
-    }
+	render() {
+		return (
+			<ScrollView style={{padding: 20}}>
+				<Text style={{fontSize: 27}}>Login</Text>
+				<TextInput placeholder="Username" />
+				<TextInput placeholder="Password" />
+				<View style={{margin: 7}} />
+				<Button onPress={this.props.onLoginPress} title="Submit" />
+			</ScrollView>
+		);
+	}
 }
