62e59d61da20537be62b39d2b184a2fe96977abb
saladsnake <mifrent@hotmail.com>
2018-09-07 08:47:53 +1000

    add buttons styling

--- Containers/Header.js
+++ Containers/Header.js
@@ -1,28 +1,30 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {ScrollView, View, Text, Button, TouchableOpacity} from 'react-native';
+import {Icon} from 'react-native-elements'
 
 
 class HeaderBanner extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-		};
-	}
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
 
-  render() {
-    return (
-      <View style={{
-          flexDirection: 'row',
-          justifyContent: 'space-between'
-        }}>
-        <Button title="Gear" onPress={this.props.onProfilePress}></Button>
-        <Text>
-          Open Shift
-        </Text>
-        <Button title="More" onPress={this.props.onOSPress}></Button>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={{
+                flexDirection: 'row',
+                justifyContent: 'space-between'
+            }}>
+
+                <Icon reverse color='#517fa4' name='settings' onPress={this.props.onProfilePress}/>
+                
+                <Icon reverse color='#517fa4' name='home' onPress={this.props.onHomePress}/>
+
+                <Icon reverse color='#517fa4' name='control-point' onPress={this.props.onOSPress}/>
+
+            </View>
+        );
+    }
 }
 
 export default HeaderBanner;
